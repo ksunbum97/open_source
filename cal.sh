3c5c1fae2c8fceb1cc1af971d969f186b4915a83
ksunbum97 <ksunbum97@gmail.com>
2022-03-27 22:12:04 +0900

    first commit

--- cal.sh
+++ cal.sh
@@ -4,39 +4,65 @@
 num1=`cat num1.txt`
 num2=`cat num2.txt`
 
-echo "...non operator parameter..."
+if [ $# -eq 0 ]; then
+	echo "...non operator parameter..."
 
-echo "1) add"
-echo "2) sub"
-echo "3) div"
-echo "4) mul"
-printf "select menu:"
-read menu
+	echo "1) add"
+	echo "2) sub"
+	echo "3) div"
+	echo "4) mul"
+	printf "select menu:"
+	read menu
 
-if [ ${menu} -eq 1 ];then
-	ans=`expr $num1 + $num2`
-	op="add"
-elif [ ${menu} -eq 2 ];then
-	ans=`expr $num1 - $num2` 
-	op="sub"
-elif [ ${menu} -eq 3 ];then
-	ans=`expr $num1 / $num2`
-	op="div"
-elif [ ${menu} -eq 4 ];then
-	ans=`expr $num1 \* $num2`
-	op="mul"
+	if [ ${menu} -eq 1 ];then
+		ans=`expr $num1 + $num2`
+		op="add"
+	elif [ ${menu} -eq 2 ];then
+		ans=`expr $num1 - $num2` 
+		op="sub"
+	elif [ ${menu} -eq 3 ];then
+		ans=`expr $num1 / $num2`
+		op="div"
+	elif [ ${menu} -eq 4 ];then
+		ans=`expr $num1 \* $num2`
+		op="mul"
+	fi
+	
+	echo ""
+	printf "num1:"
+	echo $num1
+	printf "num2:"
+	echo $num2
+	printf "op:"
+	echo ${op}
+	printf "result:"
+	echo $ans
+else
+	menu=$1
+	if [ $1 == "1" ];then
+                ans=`expr $num1 + $num2`
+                op="add"
+        elif [ "${menu}" == "2" ];then
+                ans=`expr $num1 - $num2`
+                op="sub"
+        elif [ "${menu}" == "3" ];then
+                ans=`expr $num1 / $num2`
+                op="div"
+        elif [ "${menu}" == "4" ];then
+                ans=`expr $num1 \* $num2`
+                op="mul"
+        fi
+
+        echo "...${op} selected..."
+        echo "...run calculater..."
+
+        echo ""
+        printf "num1:"
+        echo $num1
+        printf "num2:"
+        echo $num2
+        printf "op:"
+        echo $op
+        printf "result:"
+        echo $ans
 fi
-
-if [ -z "$OPERATOR" ];then
-	echo "...${op} selected..."
-	echo "...run calculater..."
-fi
-echo ""
-printf "num1:"
-echo $num1
-printf "num2:"
-echo $num2
-printf "op:"
-echo ${op}
-printf "result:"
-echo $ans
--- myprog.sh
+++ myprog.sh
@@ -7,11 +7,15 @@ cp cal.sh ./temp/tmp_cal.sh
 cp num1.txt ./temp/num1.txt
 cp num2.txt ./temp/num2.txt
 echo "...copy files to temp directory..."
-OPERATOR="op"
 
-#export OPERATOR
+echo "1) add"
+echo "2) sub"
+echo "3) div"
+echo "4) mul"
+printf "select menu:"
+read menu
 
-./temp/tmp_cal.sh $OPERATOR
+./temp/tmp_cal.sh ${menu}
 
 rm temp/tmp_cal.sh
 rm temp/num1.txt
