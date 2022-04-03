9149ccd19ed77c19e1859a58c66b4a715376451b
ksunbum97 <48879588+ksunbum97@users.noreply.github.com>
2022-04-03 15:52:32 +0900

    value to array

--- cal.sh
+++ cal.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-#calculate integers in textfiles
+#calculate integers in textfile
 
+declare -a num_arr
 num1=`cat num1.txt`
 num2=`cat num2.txt`
+num_arr[0]=$num1
+num_arr[1]=$num2
 
 if [ $# -eq 0 ]; then
 	echo "...non operator parameter..."
@@ -15,54 +18,54 @@ if [ $# -eq 0 ]; then
 	read menu
 
 	if [ ${menu} -eq 1 ];then
-		ans=`expr $num1 + $num2`
+		ans=`expr ${num_arr[0]} + ${num_arr[1]}`
 		op="add"
 	elif [ ${menu} -eq 2 ];then
-		ans=`expr $num1 - $num2` 
+		ans=`expr ${num_arr[0]} - ${num_arr[1]}` 
 		op="sub"
 	elif [ ${menu} -eq 3 ];then
-		ans=`expr $num1 / $num2`
+		ans=`expr ${num_arr[0]} / ${num_arr[1]}`
 		op="div"
 	elif [ ${menu} -eq 4 ];then
-		ans=`expr $num1 \* $num2`
+		ans=`expr ${num_arr[0]} \* ${num_arr[1]}`
 		op="mul"
 	fi
 	
 	echo ""
 	printf "num1:"
-	echo $num1
+	echo ${num_arr[0]}
 	printf "num2:"
-	echo $num2
+	echo ${num_arr[1]}
 	printf "op:"
 	echo ${op}
 	printf "result:"
 	echo $ans
 else
 	menu=$1
-	if [ $1 == "1" ];then
-                ans=`expr $num1 + $num2`
-                op="add"
-        elif [ "${menu}" == "2" ];then
-                ans=`expr $num1 - $num2`
-                op="sub"
-        elif [ "${menu}" == "3" ];then
-                ans=`expr $num1 / $num2`
-                op="div"
-        elif [ "${menu}" == "4" ];then
-                ans=`expr $num1 \* $num2`
-                op="mul"
-        fi
+	if [ ${menu} -eq 1 ];then
+		ans=`expr ${num_arr[0]} + ${num_arr[1]}`
+		op="add"
+	elif [ ${menu} -eq 2 ];then
+		ans=`expr ${num_arr[0]} - ${num_arr[1]}` 
+		op="sub"
+	elif [ ${menu} -eq 3 ];then
+		ans=`expr ${num_arr[0]} / ${num_arr[1]}`
+		op="div"
+	elif [ ${menu} -eq 4 ];then
+		ans=`expr ${num_arr[0]} \* ${num_arr[1]}`
+		op="mul"
+	fi
 
         echo "...${op} selected..."
         echo "...run calculater..."
 
-        echo ""
-        printf "num1:"
-        echo $num1
-        printf "num2:"
-        echo $num2
-        printf "op:"
-        echo $op
+	echo ""
+	printf "num1:"
+	echo ${num_arr[0]}
+	printf "num2:"
+	echo ${num_arr[1]}
+	printf "op:"
+	echo ${op}
         printf "result:"
         echo $ans
 fi
